054c7719802d2c39fa52852a34e472a42dfa271a
zyLiu6707 <31042884+zyLiu6707@users.noreply.github.com>
2020-12-05 14:29:12 -0500

    add a explicit constructor

--- error_flag.h
+++ error_flag.h
@@ -3,14 +3,32 @@
 
 class ErrorFlag {
  public:
-  bool err_redundant_identical_ctrl_arg_flag = false;
-  bool err_redundant_total_pull_time_ctrl_arg_flag = false;
-  bool err_redundant_pity_ctrl_arg_flag = false;
-  bool err_conflict_ctrl_arg_flag = false;
-  bool err_invalid_value_for_ctrl_arg_total_pull_time_flag = false;
-  bool err_missing_value_for_ctrl_arg_total_pull_time_flag = false;
-  bool err_invalid_value_for_ctrl_arg_pity_flag = false;
-  bool err_missing_value_for_ctrl_arg_pity_flag = false;
+  // The default values for these flags are false
+  bool err_redundant_identical_ctrl_arg_flag;
+  bool err_redundant_total_pull_time_ctrl_arg_flag;
+  bool err_redundant_pity_ctrl_arg_flag;
+  bool err_conflict_ctrl_arg_flag;
+  bool err_invalid_value_for_ctrl_arg_total_pull_time_flag;
+  bool err_missing_value_for_ctrl_arg_total_pull_time_flag;
+  bool err_invalid_value_for_ctrl_arg_pity_flag;
+  bool err_missing_value_for_ctrl_arg_pity_flag;
+
+  ErrorFlag(bool _err_redundant_identical_ctrl_arg_flag,
+            bool _err_redundant_total_pull_time_ctrl_arg_flag,
+            bool _err_redundant_pity_ctrl_arg_flag,
+            bool _err_conflict_ctrl_arg_flag,
+            bool _err_invalid_value_for_ctrl_arg_total_pull_time_flag,
+            bool _err_missing_value_for_ctrl_arg_total_pull_time_flag,
+            bool _err_invalid_value_for_ctrl_arg_pity_flag,
+            bool _err_missing_value_for_ctrl_arg_pity_flag) : 
+        err_redundant_identical_ctrl_arg_flag(err_redundant_identical_ctrl_arg_flag),
+        err_redundant_total_pull_time_ctrl_arg_flag(_err_redundant_total_pull_time_ctrl_arg_flag),
+        err_redundant_pity_ctrl_arg_flag(_err_redundant_pity_ctrl_arg_flag),
+        err_conflict_ctrl_arg_flag(_err_conflict_ctrl_arg_flag),
+        err_invalid_value_for_ctrl_arg_total_pull_time_flag(_err_invalid_value_for_ctrl_arg_total_pull_time_flag),
+        err_missing_value_for_ctrl_arg_total_pull_time_flag(_err_missing_value_for_ctrl_arg_total_pull_time_flag),
+        err_invalid_value_for_ctrl_arg_pity_flag(_err_invalid_value_for_ctrl_arg_pity_flag),
+        err_missing_value_for_ctrl_arg_pity_flag(_err_missing_value_for_ctrl_arg_pity_flag) {}
 };
 
 #endif  // ERROR_FLAG_H
